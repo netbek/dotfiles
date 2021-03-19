2044f45b70f63bed64c811112bf8aeeda109a1ee
Hein Bekker <hein@siyavula.com>
2021-03-19 10:32:34 +0200

    Add pylance config

--- install.sh
+++ install.sh
@@ -28,6 +28,25 @@ info "Installing programs ..."
 sudo apt update
 sudo apt install code r-base
 
+code --install-extension bpruitt-goddard.mermaid-markdown-syntax-highlighting
+code --install-extension DavidAnson.vscode-markdownlint
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension eamodio.gitlens
+code --install-extension esbenp.prettier-vscode
+code --install-extension glen-84.sass-lint
+code --install-extension jakeboone02.mediawiki
+code --install-extension joaompinto.asciidoctor-vscode
+code --install-extension lextudio.restructuredtext
+code --install-extension ms-mssql.mssql
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension PhilHindle.errorlens
+code --install-extension richie5um2.vscode-sort-json
+code --install-extension ronnidc.nunjucks
+code --install-extension samuelcolvin.jinjahtml
+code --install-extension torn4dom4n.latex-support
+code --install-extension vstirbu.vscode-mermaid-preview
+
 success "Programs installed"
 
 info "Installing configurations ..."
